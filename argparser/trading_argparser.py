3d15095702cf618b41bd91f9ebdd7c96d978dcbd
yite <yite@icloud.com>
2024-11-02 00:13:24 +0800

    init trader

--- argparser/trading_argparser.py
+++ argparser/trading_argparser.py
@@ -1,6 +1,7 @@
 import argparse
 import time
-import helper.utils as utils
+import redis
+import helper
 import helper.logging as logging
 from binance.client import Client as BinanceClient
 from trader.data import query_kline
@@ -18,14 +19,17 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-def main(binance_client: BinanceClient, config: dict):
-    symbol = config["symbol"]
-    granular = config["granular"]
-    if config["family"] == "pythagoras":
+def main(binance_client: BinanceClient, r: redis.Redis, trading_config: dict):
+    symbol = trading_config["symbol"]
+    granular = trading_config["granular"]
+    if trading_config["family"] == "pythagoras":
         tr = Pythagoras()
     while True:
         # Get the latest kline data
-        
+        current_time = int(time.time() * 1000)
+        klines = query_kline(r, symbol, granular, current_time - helper.to_unixtime_interval(granular) * 90, current_time)
+        logger.info(f"num of kline data: {len(klines)}")
+        logger.info(f"latest kline data: {klines[-1]}")
         # Wait
         time.sleep(1)
 
@@ -34,13 +38,16 @@ if __name__ == "__main__":
     logger = logging.getLogger("trading")
     args = parse_arguments()
     try:
-        config = utils.load_config(args.config)
+        config = helper.load_config(args.config)
     except Exception as e:
         logger.error(f"Error loading configuration: {e}")
         raise
     logger.info("Configuration loaded successfully")
     binance_config = config["binance"]
     binance_client = BinanceClient(binance_config["api_key"], binance_config["api_secret"])
+    redis_config = config["redis"] 
+    r = redis.Redis(host=redis_config["host"], port=redis_config["port"], db=redis_config)
     trading_config = config["trading"]
-    main(binance_client, config)
+
+    main(binance_client, r, trading_config)
 
--- trader/strategy/pythagoras.py
+++ trader/strategy/pythagoras.py
@@ -1,3 +1,41 @@
+import binance.client as BinanceClient
+import helper.logging as logging
+from trader.data import KLine
+
+logger = logging.getLogger("trading")
+
 class Pythagoras:
-    def __init__(self):
+    def __init__(self, binance_client: BinanceClient, trading_config: dict):
+        self.client = binance_client
+        self.symbol = trading_config["symbol"]
+
+        self.balance = {
+            self.base: 0,
+            self.quote: 0
+        }
+
+    @property
+    def base(self):
+        return self.symbol[-3:]
+    
+    @property
+    def quote(self):
+        return self.quote[:-3]
+    
+    def get_balance(self):
+        logger.info(f"{self.base} balance = {self.client.get_asset_balance(asset=self.base)}")
+        # self.client.get_asset_balance(asset=self.quote)
+
+    def invoke(self, data: list[KLine]):
+        pass
+
+    def is_buy(self):
+        return True
+    
+    
+    def is_sell(self):
+        return False
+    
+    @staticmethod
+    def to_dataframe(data: list[KLine]):
         pass

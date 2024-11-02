3fb68085590e0590eb8b86493bca8b003bed2316
yite <yite@icloud.com>
2024-11-02 16:36:02 +0800

    trading process

--- trader/strategy/notification.py
+++ trader/strategy/notification.py
@@ -6,3 +6,26 @@ def on_trading_start(family: str, **kwargs) -> dict:
         "color": 65280,  # Green color
     }
 
+def on_trading_finish(family: str, **kwargs) -> dict:
+    description = f"**Family**: {family}\n" + "\n".join([f"**{key}**: \t{value}" for key, value in kwargs.items()])
+    return {
+        "title": "On Trading Finish",
+        "description": description,
+        "color": 65280,  # Green color
+    }
+
+def on_order_sent(family: str, **kwargs) -> dict:
+    description = f"**Family**: {family}\n" + "\n".join([f"**{key}**: \t{value}" for key, value in kwargs.items()])
+    return {
+        "title": "On Order Sent",
+        "description": description,
+        "color": 16776960,  # Yellow color
+    }
+
+def on_error(family: str, error: str, **kwargs) -> dict:
+    description = f"**Family**: {family}\n" + f"**Error**: {error}\n" + "\n".join([f"**{key}**: \t{value}" for key, value in kwargs.items()])
+    return {
+        "title": "On Error",
+        "description": description,
+        "color": 16711680,  # Red color
+    }
--- trader/strategy/pythagoras.py
+++ trader/strategy/pythagoras.py
@@ -1,28 +1,32 @@
-import sys
+import time
 import pandas as pd
+from decimal import Decimal, ROUND_DOWN
 import binance.client as BinanceClient
 import helper.logging as logging
 import helper
-from .notification import on_trading_start
+from .notification import (
+    on_trading_start, on_trading_finish, on_order_sent, on_error
+)
 from trader.data import KLine
 
 logger = logging.getLogger("trading")
 
 class Pythagoras:
     def __init__(self, binance_client: BinanceClient, trading_config: dict, webhook_url: str):
+        self.family = "Pythagoras"
         self.client = binance_client
         self._id = helper.generate_random_id()
         self.symbol = trading_config["symbol"]
         self.balance = self.get_balance()
         self.webhook_url = webhook_url
-        self.trading_metadata = trading_config["meta"]
+        self.trading_meta = trading_config["meta"]
     
     def __enter__(self):
-        helper.send_notification(self.webhook_url, None, "Pythagoras", on_trading_start("Pythagoras", symbol=self.symbol, id=self._id, **self.trading_metadata))
+        helper.send_notification(self.webhook_url, None, self.family, on_trading_start(self.family, id=self._id, symbol=self.symbol, **self.trading_metadata))
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
-        helper.send_notification(self.webhook_url, None, "Pythagoras", on_trading_start("Pythagoras", symbol=self.symbol, id=self._id))
+        helper.send_notification(self.webhook_url, None, self.family, on_trading_finish(self.family, id=self._id, symbol=self.symbol))
         return self
 
     @property
@@ -43,39 +47,131 @@ class Pythagoras:
             elif _filter['filterType'] == 'PRICE_FILTER':
                 metadata['lot_size'] = _filter['tickSize'].rstrip('0')
 
-        return metadata
-    
-    def get_askbid(self):
-        return self.client.get_order_book(symbol=self.symbol, limit=1)
-    
+        return metadata    
     
     def get_balance(self):
-        balance = {
-            self.base: float(self.client.get_asset_balance(asset=self.base)['free']),
-            self.quote: float(self.client.get_asset_balance(asset=self.quote)['free'])
-        }
+        user_assets = self.client.get_user_asset(needBtcValuation=True)
+        balance = {}
+        for asset in user_assets:
+            balance[asset['asset']] = str(Decimal(asset['free']) + Decimal(asset['locked']))
+
         logger.info(f"Current balance: {balance}")
         return balance
+    
+    def get_askbid(self)-> tuple[str, str]:
+        order_book = self.client.get_order_book(symbol=self.symbol, limit=1)
+        return order_book['asks'][0][0], order_book['bids'][0][0]
 
     def invoke(self, data: list[KLine]):
+        if not data:
+            logger.error("No data to analyze in invoke method")
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_error(self.family, id=self._id, error="No data to analyze in invoke method")
+                                     )
+            return
+        if not self.is_updated(data[-1].open_time):
+            logger.error(f"Data is not updated: {data[-1].to_dict()}")
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_error(self.family, id=self._id, error="Data is not updated")
+                                     )
+            return
         df = self.to_dataframe(data)
-        return
-        
+        df_analysis = self.analyze(df)
+        if self.is_buy(df_analysis):
+            kline = data[-1]
+            ask, _ = self.get_askbid()
+            sz = Decimal(self.balance[self.base]) / Decimal(ask)
+            self.market_buy(str(sz))
+        elif self.is_sell():
+            self.market_sell(self.balance[self.base])
 
-    def is_buy(self):
+    def analyze(self, df: pd.DataFrame):
+        df_analysis = df.copy()
+        df_analysis['fast_ma'] = df_analysis['close'].rolling(window=self.trading_meta["fast_ma_period"]).mean()
+        df_analysis['slow_ma'] = df_analysis['close'].rolling(window=self.trading_meta["slow_ma_period"]).mean()
+        df_analysis['slow_ma.diff'] = df_analysis['slow_ma'].diff()
+        return df_analysis.dropna()
+        
+    def is_buy(self, df: pd.DataFrame):
+        """
+        Buy Logic:
+            If slow_ma.diff > 0, that means the trend is going up, then we find a price is lower enough to buy.
+        """
+        last_update = df.tail(1)
+        
+        if self.is_hold(last_update['close'].values[0]):
+            return False
+        if last_update['slow_ma.diff'].values[0] < 0:
+            return False
+        if last_update['fast_ma'].values[0] > last_update['slow_ma'].values[0]:
+            return False
+        if last_update['close'].values[0] > last_update['fast_ma'].values[0]:
+            return False
+        df_condition = df[df['fast_ma'] > df['close']]
+        if df_condition.empty:
+            return False
+        df_condition['drawdown'] = (df_condition['fast_ma'] - df_condition['close']) / df_condition['fast_ma']
+        q = df_condition['drawdown'].quantile(0.25) # q is negative
+        logger.info(f"Trigger buy px should below {last_update['fast_ma'].values[0] * (1 + q)}")
+        if last_update['close'].values[0] > last_update['fast_ma'].values[0] * (1 + q):
+            return False
         return True
     
-    def buy(self, price: float, quantity: float):
-        pass
+    def is_sell(self, df: pd.DataFrame):
+        last_update = df.tail(1)
+        if not self.is_hold(last_update['close'].values[0]):
+            return False
+        if last_update['slow_ma.diff'].values[0] < 0:
+            logger.warning("Stop loss triggered")
+            return True # stop loss
+        if last_update['fast_ma'].values[0] < last_update['slow_ma'].values[0]:
+            return False
+        df_condition = df[df['fast_ma'] < df['close']]
+        if df_condition.empty:
+            return False
+        df_condition['gain'] = (df_condition['close'] - df_condition['fast_ma']) / df_condition['fast_ma']
+        q = df_condition['gain'].quantile(0.25)
+        logger.info(f"Trigger buy px should below {last_update['fast_ma'].values[0] * (1 + q)}")
+        if last_update['close'].values[0] < last_update['fast_ma'].values[0] * (1 + q):
+            return False
+        return True
+
     
-    def sell(self, price: float, quantity: float):
-        pass
-    
-    def is_sell(self):
+    def is_hold(self, px: float):
+        if self.balance[self.quote] * px > self.balance[self.base]:
+            return True
         return False
     
-    def stop(self):
-        pass
+    @staticmethod
+    def is_updated(ts: int, recv_window: int = 10_000):
+        current_time = int(time.time() * 1000)
+        return ts > current_time - recv_window
+    
+    def market_buy(self, quantity: float):
+        qty = Decimal(quantity).quantize(Decimal(self.exchange_metadata['lot_size']), rounding=ROUND_DOWN)
+        try:
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_order_sent(self.family, id=self._id, symbol=self.symbol, side="BUY", quantity=str(qty))
+                                     )
+            self.client.order_market_buy(symbol=self.symbol, quantity=str(qty))
+        except Exception as e:
+            logger.error(f"Failed to place market buy order: {e}")
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_error(self.family, id=self._id, error=e)
+                                     )            
+    
+    def market_sell(self, quantity: float):
+        qty = Decimal(quantity).quantize(Decimal(self.exchange_metadata['lot_size']), rounding=ROUND_DOWN)
+        try:
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_order_sent(self.family, id=self._id, symbol=self.symbol, side="SELL", quantity=str(qty))
+                                     )
+            self.client.order_market_buy(symbol=self.symbol, quantity=str(qty))
+        except Exception as e:
+            logger.error(f"Failed to place market buy order: {e}")
+            helper.send_notification(self.webhook_url, None, self.family, 
+                                     on_error(self.family, id=self._id, error=e)
+                                     )
     
     @staticmethod
     def to_dataframe(data: list[KLine]):

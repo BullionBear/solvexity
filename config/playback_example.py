d349177373a682d93e8e88d8a5b84d5f9e75beb5
yite <coastq22889@icloud.com>
2024-11-07 00:24:11 +0800

    add playback example

---
+++ config/playback_example.py
@@ -0,0 +1,13 @@
+{
+    "redis": {
+        "host": "localhost",
+        "port": 6379,
+        "db": 0
+    },
+    "data": {
+        "url": "postgresql://username:password@35.229.134.70:5432/solvexity",
+        "symbol": "BTCUSDT",
+        "granular": "1d",
+        "limit": 90
+    },
+}

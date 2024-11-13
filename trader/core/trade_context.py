bb0cde61801888230299eda7ee763c7456510e8b
yite <coastq22889@icloud.com>
2024-11-13 09:57:25 +0800

    trader context

--- trader/core/trade_context.py
+++ trader/core/trade_context.py
@@ -1,10 +1,56 @@
-class TradeContext:
-    def __init__(self):
+from abc import ABC, abstractmethod
+from decimal import Decimal
+import redis
+from trader.data import query_latest_kline, get_key
+
+class TradeContext(ABC):
+    @abstractmethod
+    def market_buy(self, symbol: str, size: float):
         pass
 
-class BacktestContext(TradeContext):
-    def __init__(self):
+    @abstractmethod
+    def market_sell(self, symbol: str, size: float):
         pass
+
+    @abstractmethod
+    def get_balance(self, token: str):
+        pass
+
+    @abstractmethod
+    def get_askbid(self, symbol: str):
+        pass
+
+
+class BacktestContext(TradeContext):
+    """
+    A backtest context for trading strategies.  The execution of trades is simulated in this context in simple strategies.
+    """
+    def __init__(self, init_balance: dict, granular: str, redis: redis.Redis):
+        """
+        Args:
+            init_balance (dict): The initial balance for the backtest context, e.g. {"BTC": '1', "USDT": '10000'}
+        """
+        self.ts = 0
+        self.balance = {k: Decimal(v) for k, v in init_balance.items()}
+
+    def market_buy(self, symbol: str, size: Decimal):
+        ask, _ = self.get_askbid(symbol)
+        base, quote = symbol[:-4], symbol[-4:] # e.g. BTCUSDT -> BTC, USDT
+        self.balance[base] += size
+        self.balance[quote] -= size * ask
+
+    def market_sell(self, symbol: str, size: Decimal):
+        _, bid = self.get_askbid(symbol)
+        base, quote = symbol[:-4], symbol[-4:]
+        self.balance[base] -= size
+        self.balance[quote] += size * bid
+
+    def get_balance(self, token: str):
+        return self.balance[token]
+    
+    def get_askbid(self, symbol: str):
+        lastest_kline = query_latest_kline(self.redis, symbol, "1m")
+        return 
 
 class PaperTradeContext(TradeContext):
     def __init__(self):
@@ -13,8 +59,3 @@ class PaperTradeContext(TradeContext):
 class LiveTradeContext(TradeContext):
     def __init__(self):
         pass
-
-class TradeContextFactory:
-    @staticmethod
-    def create():
-        return TradeContext()

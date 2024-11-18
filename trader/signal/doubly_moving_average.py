68ad2462918ff781ab566101b8563f1b4a073894
yite <yite@icloud.com>
2024-11-19 07:44:13 +0800

    doubly moving average

--- trader/signal/doubly_moving_average.py
+++ trader/signal/doubly_moving_average.py
@@ -1,3 +1,5 @@
+from typing import Type
+from trader.core import TradeContext
 from trader.core import Signal, SignalType
 import pandas as pd
 
@@ -5,8 +7,9 @@ pd.options.mode.copy_on_write = True
 
 
 class DoubleMovingAverage(Signal):
-    def __init__(self, trade_context, symbol: str, fast_period: int, slow_period: int, limit: int):
+    def __init__(self, trade_context: Type[TradeContext], symbol: str, fast_period: int, slow_period: int, limit: int):
         super().__init__(trade_context)
+        self.name = "Double Moving Average"
         self.symbol = symbol
         self.fast_period = fast_period
         self.slow_period = slow_period
@@ -15,7 +18,8 @@ class DoubleMovingAverage(Signal):
     def solve(self):
         klines = self.trade_context.get_klines(self.symbol, self.limit)
         df = Signal.to_dataframe(klines)
-        self.analyze(df)
+        df_analyze = self.analyze(df)
+        
         return SignalType.HOLD
     
     def analyze(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -23,8 +27,8 @@ class DoubleMovingAverage(Signal):
         df['slow'] = df['close'].rolling(window=self.slow_period).mean()
         pass
     
-    def export(self, destination: str):
+    def export(self, output_path: str):
         pass
 
-    def visualize(self, destination: str):
+    def visualize(self, df: pd.DataFrame, output_path: str):
         pass

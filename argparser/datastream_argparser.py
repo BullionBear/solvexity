e3ae47288ef39b60df874f8add44ec00642b114c
yite <yite@icloud.com>
2024-11-23 10:20:21 +0800

    refactor data stream

--- argparser/datastream_argparser.py
+++ argparser/datastream_argparser.py
@@ -1,18 +1,23 @@
 import argparse
-import signal
-import threading
 import helper
 import helper.logging as logging
+import threading
+import signal
+import json
 import time
-from trader.data.provider import HistoricalProvider, RealtimeProvider
+import traceback
 from service import ServiceFactory
+from trader.data import get_key
+from trader.data.provider import DataProviderFactory
+from trader.context import ContextFactory
+from trader.signal import SignalFactory
 
 logging.setup_logging()
-logger = logging.getLogger("data")
+logger = logging.getLogger("trading")
 shutdown_event = threading.Event()
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Read configuration and run playback process")
+    parser = argparse.ArgumentParser(description="Read configuration and run trading process")
     parser.add_argument(
         "-c", "--config",
         type=str,
@@ -21,54 +26,54 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-def handle_shutdown(signum, frame):
-    """Signal handler for graceful shutdown."""
-    logger.info("Shutdown signal received, stopping...")
-    shutdown_event.set()  # Trigger shutdown event to stop main loop
+def handle_shutdown_signal(signum, frame):
+    logger.info("Shutdown signal received. Shutting down gracefully...")
+    shutdown_event.set()
 
+def heartbeat(redis, interval):
+    while not shutdown_event.is_set():
+        message = json.dumps({"type": "ping", "timestamp": int(time.time()) * 1000})
+        redis.publish("heartbeat", message)
+        time.sleep(interval)  # Send a ping every `interval` seconds
+    message = json.dumps({"type": "ping", "timestamp": int(time.time()) * 1000})
+    redis.publish("heartbeat", message)
+    logger.info("Heartbeat thread terminated.")
 
-def main(service_config: dict, trigger_config: dict, data_config: dict):
-    service = ServiceFactory(service_config)
-    if data_config["provider"] == "history":
-        r = data_config["redis"]
-        sql = data_config["sql_engine"]
-        provider = HistoricalProvider(
-            service[r], service[sql], data_config["symbol"],
-            data_config["granular"], data_config["start"], data_config["end"],
-            data_config["limit"]
-        )
-    elif data_config["provider"] == "realtime":
-        r = data_config["redis"]
-        provider = RealtimeProvider(
-            service[r], data_config["symbol"], data_config["granular"], data_config["limit"]
-        )
-    else:
-        raise ValueError(f"Unknown data provider: {data_config['provider']}")
-    signal.signal(signal.SIGINT, lambda signum, frame: provider.stop())
-    signal.signal(signal.SIGTERM, lambda signum, frame: provider.stop())
-    sleep_time = trigger_config["sleep"] / 1000
-    for kline in provider:
-        if shutdown_event.is_set():
-            break
-        logger.info(f"Publish kline data: {kline}")
-        time.sleep(sleep_time)
-    
+def main(services_config: dict, data_config:dict, context_config: dict, signal_config: dict):
+    services = ServiceFactory(services_config)
+    contexts = ContextFactory(services, context_config)
+    signals = SignalFactory(contexts, signal_config)
+    providers = DataProviderFactory(services, data_config)
 
+    provider = providers["realtime_provider"]
+
+    # Start provider in a controlled loop
+    try:
+        for data in provider:
+            if shutdown_event.is_set():
+                break
+            logger.info(f"Publish kline data: {data}")
+    finally:
+        provider.stop()
+
+    logger.info("Trading process terminated gracefully.")
 
 if __name__ == "__main__":
     # Register signal handler for graceful shutdown
-    signal.signal(signal.SIGINT, handle_shutdown)
-    signal.signal(signal.SIGTERM, handle_shutdown)
-
+    signal.signal(signal.SIGINT, handle_shutdown_signal)
+    signal.signal(signal.SIGTERM, handle_shutdown_signal)
+    
     args = parse_arguments()
     try:
         config = helper.load_config(args.config)
     except Exception as e:
         logger.error(f"Error loading configuration: {e}")
-        raise    
+        raise
+    
     logger.info("Configuration loaded successfully")
 
     try:
-        main(config["services"], config["trigger"], config["data"])
+        main(config["services"], config["data"], config["contexts"], config["signals"])
     except Exception as e:
-        logger.error(f"Unexpected error: {e}")
+        full_traceback = traceback.format_exc()
+        logger.error(f"Error invoking strategy: {e}\n{full_traceback}")

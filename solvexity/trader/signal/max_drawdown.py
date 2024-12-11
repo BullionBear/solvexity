15216c6c0c2bc8192b2e38ff69545d433761d38d
yite <yite@icloud.com>
2024-12-11 22:09:40 +0800

    max drawdown

--- solvexity/trader/signal/max_drawdown.py
+++ solvexity/trader/signal/max_drawdown.py
@@ -16,28 +16,39 @@ class MaxDrawdown(Signal):
     MaxDrawdown only return HOLD and BUY signals.
     """
     NAME = "Maximal Drawdown"
-    def __init__(self, trade_context: Type[TradeContext], symbol: str, rollback_period: int, threshold: float, limit: int):
+    def __init__(self, trade_context: Type[TradeContext], symbol: str, rollback_period: int, threshold: float, granular: str):
         super().__init__(trade_context)
         self.symbol: str = symbol
         self.rollback_period: int = rollback_period
+        self.granular: str = granular
         self.threshold: float = threshold
-        self.limit: int = limit
 
         self.df_analyze: pd.DataFrame = None
 
     def solve(self) -> SignalType:
         # Retrieve historical market data
-        klines = self.trade_context.get_klines(self.symbol, self.limit)
+        klines = self.trade_context.get_klines(self.symbol, self.rollback_period, self.granular)
         df = Signal.to_dataframe(klines)
         # Analyze the data to calculate moving averages
         self.df_analyze = self.analyze(df)
         return SignalType.BUY
 
     def analyze(self, df: pd.DataFrame) -> pd.DataFrame:
-        if len(df) < self.slow_period:
+        if len(df) < self.rollback_period:
             logger.warning(f"Insufficient data points for analysis. Expected at least {self.slow_period} but receive {len(df)}.")
             return df
         df_analyze = df.copy()
+        high_data = df_analyze[['open_time', 'high']].copy()
+        high_data['is_high'] = True
+        high_data.rename(columns={'high': 'price'}, inplace=True)
+        low_data = df_analyze[['open_time', 'low']].copy()
+        low_data['is_high'] = False
+        low_data.rename(columns={'low': 'price'}, inplace=True)
+        transformed_df = pd.concat([high_data, low_data], axis=0)
+        transformed_df.sort_values(by=['open_time', 'is_high'], ascending=[True, False], inplace=True)
+        transformed_df['cumulative_max'] = transformed_df['price'].cummax()
+        transformed_df['drawdown'] = (transformed_df['cumulative_max'] - transformed_df['price']) / transformed_df['cumulative_max']
+
         df_analyze['fast'] = df_analyze['close'].rolling(window=self.fast_period).mean()
         df_analyze['slow'] = df_analyze['close'].rolling(window=self.slow_period).mean()
         return df_analyze

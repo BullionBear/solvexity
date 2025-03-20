88fb74f628110a42254b4ca2d786a6e2abc87a3d
yite <yite@icloud.com>
2025-03-20 10:42:35 +0800

    structure grpc

--- scripts/grpc.sh
+++ scripts/grpc.sh
@@ -8,12 +8,16 @@ codegen() {
     echo "Generate pb2 grpc from proto..."
     # Loop through each .proto file and generate Python files
     for proto_file in "$PROTO_DIR"/*.proto; do
+        proto_name=$(basename "$proto_file" .proto)
+        proto_out_dir="$OUT_DIR/$proto_name"
+        mkdir -p "$proto_out_dir"
+        
         echo "Generating files for $proto_file..."
-        python -m grpc_tools.protoc -I="$PROTO_DIR" --python_out="$OUT_DIR" --grpc_python_out="$OUT_DIR" "$proto_file"
+        python -m grpc_tools.protoc -I="$PROTO_DIR" --python_out="$proto_out_dir" --grpc_python_out="$proto_out_dir" "$proto_file"
     done
     
     # Fix imports in *_pb2_grpc.py files
-    for grpc_file in "$OUT_DIR"/*_pb2_grpc.py; do
+    for grpc_file in "$OUT_DIR"/*/*_pb2_grpc.py; do
         echo "Fixing imports in $grpc_file..."
         sed -i 's/import \(.*_pb2\) as/from . import \1 as/' "$grpc_file"
     done

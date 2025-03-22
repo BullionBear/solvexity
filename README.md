09939e8350458c2ff7f9e83491659d0ca440994b
yite <yite@icloud.com>
2025-03-22 13:37:52 +0800

    unittest and integration test

--- README.md
+++ README.md
@@ -11,7 +11,17 @@ python -m solvexity.main
 ```
 
 ## Unittest & Integration Test
+### Unittest
+```
+pytest -m "not integration"
+```
+
 ### Integration test
+For each test in integration test, developer need to add decorator like:
+```
+@pytest.mark.integration
+def test_request_sql_klines(feed):
+```
 
 Start local resource
 ```
@@ -21,4 +31,9 @@ docker compose up -d
 Run integration test
 ```
 pytest -m integration
+```
+
+### Run all tests
+```
+pytest 
 ```
---
+++ tests/analytic/test_feed.py
@@ -0,0 +1,18 @@
+import pytest
+from unittest.mock import MagicMock
+from solvexity.analytic.feed import Feed
+
+
+@pytest.fixture
+def mock_feed():
+    mock_redis = MagicMock()
+    mock_sql_engine = MagicMock()
+    return Feed(cache=mock_redis, sql_engine=mock_sql_engine)
+
+
+def test_get_cache_key(mock_feed):
+    symbol = "BTCUSDT"
+    interval = "1m"
+    expected = "BTCUSDT-1m"
+    actual = mock_feed._get_cache_key(symbol, interval)
+    assert actual == expected

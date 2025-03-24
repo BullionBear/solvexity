756ebb2698b34e528a88c91c6ced170bdc207406
yite <yite@icloud.com>
2025-03-24 16:58:19 +0800

    use grpc testing

--- solvexity/client/greet_client.py
+++ solvexity/client/greet_client.py
@@ -3,7 +3,7 @@ import solvexity.generated.greet.greet_pb2 as greet_pb2
 import solvexity.generated.greet.greet_pb2_grpc as greet_pb2_grpc
 
 def run():
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("localhost:50054") as channel:
         stub = greet_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(greet_pb2.HelloRequest(name="Alice"))
         print("Greeter client received:", response.message)

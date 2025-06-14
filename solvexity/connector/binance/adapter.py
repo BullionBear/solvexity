2e407f693d7d0740e430edbbe02b3821ca9ef33d
yite <coastq22889@icloud.com>
2025-06-14 09:44:08 +0800

    binance adapter

--- solvexity/connector/binance/adapter.py
+++ solvexity/connector/binance/adapter.py
@@ -35,10 +35,23 @@ class BinanceRestAdapter(ExchangeRestConnector):
         ) for ohlcv in ohlcvs]
     
     async def get_orderbook(self, symbol: Symbol, depth: int = 20) -> OrderBook:
-        return await self.rest_client.get_orderbook(symbol.base_asset + symbol.quote_asset, depth)
+        orderbook = await self.rest_client.get_orderbook(symbol.base_asset + symbol.quote_asset, depth)
+        return OrderBook(
+            symbol=symbol,
+            bids=[(Decimal(bid[0]), Decimal(bid[1])) for bid in orderbook['bids']],
+            asks=[(Decimal(ask[0]), Decimal(ask[1])) for ask in orderbook['asks']]
+        )
     
     async def get_trades(self, symbol: Symbol, limit: int = 100) -> List[Trade]:
-
+        trades = await self.rest_client.get_trades(symbol.base_asset + symbol.quote_asset, limit)
+        return [Trade(
+            symbol=symbol,
+            price=Decimal(trade[0]),
+            quantity=Decimal(trade[1]),
+            time=trade[2],
+            is_buyer_maker=trade[3],
+            is_best_match=trade[4]
+        ) for trade in trades]
 
 class BinanceWebSocketAdapter(ExchangeWebSocketConnector):
     def __init__(self, api_key: str, api_secret: str, use_testnet: bool = False):

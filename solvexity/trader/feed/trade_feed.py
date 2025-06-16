b08d784e39e9e6eb99adb08e82c02fec903e01e1
yite <coastq22889@icloud.com>
2025-06-16 10:03:00 +0800

    recv trade

--- solvexity/trader/feed/trade_feed.py
+++ solvexity/trader/feed/trade_feed.py
@@ -1,9 +1,48 @@
+from solvexity.connector.base import ExchangeStreamConnector
+from hooklet.base import BasePilot
+from hooklet.eventrix.v2.node import Node
+from solvexity.connector.types import Symbol, Exchange
+from typing import Callable
+from hooklet.types import HookletMessage
+from typing import AsyncGenerator
 from solvexity.connector.base import ExchangeConnector
-from hooklet.eventrix.base import BaseNode
+from solvexity.connector import ExchangeConnectorFactory
+import uuid
+import random
+import time
+from pydantic import Field
+from typing import Any
 
-class TradeFeed(BaseNode):
-    def __init__(self, connector: ExchangeConnector):
-        self.connector = connector
 
-    async def get_trades(self):
-        return await self.connector.get_trades()
+class TradeFeed(Node):
+    def __init__(self, 
+                 exchange: Exchange,
+                 pilot: BasePilot, 
+                 symbol: Symbol, 
+                 sources: list[str], 
+                 router: Callable[[HookletMessage], str | None], 
+                 node_id: None|str=None,
+                 ):
+        super().__init__(pilot, sources, router, node_id)
+        self.rest_connector = ExchangeConnectorFactory.create_rest_connector(exchange, {})
+        self.stream_connector = ExchangeConnectorFactory.create_websocket_connector(exchange, {})
+        self.symbol = symbol
+
+    async def generator_func(self) -> AsyncGenerator[HookletMessage, None]:
+        """
+        Default generator function that yields nothing.
+        Override this method to provide custom generator behavior.
+        """
+        async for trade in self.stream_connector.execution_updates_iterator(self.symbol):
+            timestamp = int(time.time() * 1000)
+            self.logger.info(f"Received Trade: {trade}")
+            yield HookletMessage(
+                id=uuid.uuid4(),
+                node_id=self.node_id,
+                correlation_id=random.randint(0, 2**64 - 1),
+                type="trade",
+                payload=trade,
+                start_at=timestamp,
+                finish_at=timestamp,
+            )
+    

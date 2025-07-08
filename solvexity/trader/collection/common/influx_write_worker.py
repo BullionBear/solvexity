a47a9c95b1e8eba4e1e89a0b52d6f42466c631f1
yite <coastq22889@icloud.com>
2025-07-08 23:34:20 +0800

    update worker

--- solvexity/trader/collection/common/influx_write_worker.py
+++ solvexity/trader/collection/common/influx_write_worker.py
@@ -1,5 +1,5 @@
 from hooklet.base.types import Job
-from hooklet.node.worker import Worker
+from hooklet.node.worker import Worker, PushPull
 from influxdb_client_3 import InfluxDBClient3, write_client_options, SYNCHRONOUS
 from solvexity.trader.payload import TradePayload
 
@@ -8,25 +8,26 @@ class InfluxWriteWorker(Worker):
                  node_id: str,
                  influxdb_url: str,
                  influxdb_database: str,
-                 influxdb_token: str
+                 influxdb_token: str,
+                 pushpull: PushPull
                  ):
-        super().__init__(node_id)
+        super().__init__(node_id, pushpull)
         self.influxdb_url = influxdb_url
         self.influxdb_database = influxdb_database
         self.influxdb_token = influxdb_token
         self.influxdb_client: InfluxDBClient3|None = None
 
-    async def process(self, job: Job) -> int:
-        if job.type == "trade":
-            self.logger.info(f"Writing trade {job.data} to InfluxDB")
-            payload = TradePayload(**job.data)
+    async def on_job(self, job: Job) -> int:
+        if job["type"] == "trade":
+            self.logger.info(f"Writing trade {job['data']} to InfluxDB")
+            payload = TradePayload(**job["data"])
             point = payload.to_point()
-            self.influxdb_client.write(point)
+            self.influxdb_client.write(record=point)
             return 0
         return -1
 
-    async def start(self) -> None:
-        await super().start()
+    async def on_start(self) -> None:
+        await super().on_start()
         wco = write_client_options(write_options=SYNCHRONOUS)
 
         self.influxdb_client = InfluxDBClient3(
@@ -36,6 +37,6 @@ class InfluxWriteWorker(Worker):
             write_client_options=wco,
         )
 
-    async def on_finish(self) -> None:
-        await super().on_finish()
+    async def on_close(self) -> None:
+        await super().on_close()
         self.influxdb_client.close()

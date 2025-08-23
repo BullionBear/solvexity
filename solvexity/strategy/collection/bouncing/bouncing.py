66ce439d4097ab30ebbd58dfcac73e323ae7d51c
yitech <coastq22889@icloud.com>
2025-08-23 16:31:51 +0800

    bouncing

--- solvexity/strategy/collection/bouncing/bouncing.py
+++ solvexity/strategy/collection/bouncing/bouncing.py
@@ -1,12 +1,34 @@
 import pandas as pd
-from solvexity.model.strategy import Bar
+from solvexity.strategy.model.bar import Bar
+from solvexity.agent.agent import Agent
 
 class Bouncing:
-    def __init__(self, n_periods: int, drawdown: float, springback: float):
+    def __init__(self, 
+        n_periods: int, 
+        drawdown: float, # 0.01 = 1%, always positive
+        springback: float, # 0.01 = 1%, always positive
+        agent: Agent
+    ):
+        # Strategy Configuration
         self.n_periods = n_periods
         self.drawdown = drawdown
         self.springback = springback
+        self.cooldown = 0
+
+        # Strategy State
         self.bars: list[Bar] = []
 
+        # Agent
+        self.agent = agent
+
+
     async def on_bar(self, bar: Bar) -> bool:
-        pass
+        self.bars.append(bar)
+        if len(self.bars) < self.n_periods:
+            return False
+        
+        df_bars = pd.DataFrame([bar.to_dict() for bar in self.bars])
+        df_bars['drawdown'] = df_bars['close'].rolling(window=self.n_periods).min()
+        df_bars['springback'] = df_bars['close'].rolling(window=self.n_periods).max()
+        df_bars['bouncing'] = df_bars['close'] > df_bars['drawdown'] and df_bars['close'] < df_bars['springback']
+        return df_bars['bouncing'].iloc[-1]

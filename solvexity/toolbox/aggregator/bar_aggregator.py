b74a40c3e67a03835627e42c2f20dbf8145e79f5
yite <coastq22889@icloud.com>
2025-09-29 09:13:30 +0800

    bar agg

--- solvexity/toolbox/aggregator/bar_aggregator.py
+++ solvexity/toolbox/aggregator/bar_aggregator.py
@@ -20,18 +20,18 @@ class TimeBarAggregator:
     def on_trade(self, trade: Trade):
         self._accumulator = trade.timestamp
         next_reference_index = int(self._accumulator // self.reference_cutoff)
-        if next_reference_index == self._reference_index:
-            self.bars[self._reference_index % self.buf_size] += trade
-        elif next_reference_index > self._reference_index:
+        if next_reference_index == self.reference_index:
+            self.bars[self.reference_index % self.buf_size] += trade
+        elif next_reference_index > self.reference_index:
             self.bars[next_reference_index % self.buf_size] = Bar.from_trade(trade)
             self.bars[next_reference_index % self.buf_size].open_time = next_reference_index * self.reference_cutoff
-            if bar := self.bars[self._reference_index % self.buf_size]:
+            if bar := self.bars[self.reference_index % self.buf_size]:
                 bar.enclose(next_reference_index * self.reference_cutoff - 1)
-                logger.info(f"Finished {self._finished_bars}'th time bar: {bar}")
-                self._finished_bars += 1
-            self._reference_index = next_reference_index
+                logger.info(f"Finished {self.finished_bars}'th time bar: {bar}")
+                self.finished_bars += 1
+            self.reference_index = next_reference_index
         else:
-            logger.warning(f"Invalid reference index: {self._reference_index} and next reference index: {next_reference_index}")
+            logger.warning(f"Invalid reference index: {self.reference_index} and next reference index: {next_reference_index}")
 
 class TickBarAggregator:
     def __init__(self, buf_size: int, reference_cutoff: int):
@@ -49,17 +49,17 @@ class TickBarAggregator:
     def on_trade(self, trade: Trade):
         self._accumulator = trade.id
         next_reference_index = int(self._accumulator // self.reference_cutoff)
-        if next_reference_index == self._reference_index:
-            self.bars[self._reference_index % self.buf_size] += trade
-        elif next_reference_index > self._reference_index:
+        if next_reference_index == self.reference_index:
+            self.bars[self.reference_index % self.buf_size] += trade
+        elif next_reference_index > self.reference_index:
             self.bars[next_reference_index % self.buf_size] = Bar.from_trade(trade)
-            if bar := self.bars[self._reference_index % self.buf_size]:
+            if bar := self.bars[self.reference_index % self.buf_size]:
                 bar.enclose(trade.timestamp)
-                logger.info(f"Finished {self._finished_bars}'th tick bar: {bar}")
-                self._finished_bars += 1
-            self._reference_index = next_reference_index
+                logger.info(f"Finished {self.finished_bars}'th tick bar: {bar}")
+                self.finished_bars += 1
+            self.reference_index = next_reference_index
         else:
-            logger.warning(f"Invalid reference index: {self._reference_index} and next reference index: {next_reference_index}")
+            logger.warning(f"Invalid reference index: {self.reference_index} and next reference index: {next_reference_index}")
 
 
 class BaseVolumeBarAggregator:
@@ -81,42 +81,42 @@ class BaseVolumeBarAggregator:
             next_reference_index = int(self._accumulator // self.reference_cutoff)
             empty_trade = trade.model_copy(deep=False)
             empty_trade.quantity = 0
-            if next_reference_index > self._reference_index:
+            if next_reference_index > self.reference_index:
                 self.bars[next_reference_index % self.buf_size] = Bar.from_trade(empty_trade)
-                self._reference_index = next_reference_index
+                self.reference_index = next_reference_index
             if abs(trade.quantity - need) < 2 * 1e-13: # trade.quantity = need                
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade
                 else:
                     bar = Bar.from_trade(trade)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 bar.enclose(trade.timestamp)
-                self._finished_bars += 1
-                logger.info(f"Finished {self._finished_bars}'th base volume bar: {bar}")
+                self.finished_bars += 1
+                logger.info(f"Finished {self.finished_bars}'th base volume bar: {bar}")
                 self._accumulator += need + 1e-13
-                self._reference_index += 1
+                self.reference_index += 1
                 trade.quantity = 0
             elif trade.quantity < need:
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade
                 else:
                     bar = Bar.from_trade(trade)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 self._accumulator += trade.quantity
                 trade.quantity = 0
                 
             elif trade.quantity > need:
                 trade_fraction = trade.model_copy(deep=False)
                 trade_fraction.quantity = need
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade_fraction
                 else:
                     bar = Bar.from_trade(trade_fraction)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 bar.enclose(trade.timestamp)
                 bar.next_id = trade.id
-                self._finished_bars += 1
-                logger.info(f"Finished {self._finished_bars}'th base volume bar: {bar}")
+                self.finished_bars += 1
+                logger.info(f"Finished {self.finished_bars}'th base volume bar: {bar}")
                 trade.quantity -= need
                 self._accumulator += need + 1e-13
             else:
@@ -143,42 +143,42 @@ class QuoteVolumeBarAggregator:
             next_reference_index = int(self._accumulator // self.reference_cutoff)
             empty_trade = trade.model_copy(deep=False)
             empty_trade.quantity = 0
-            if next_reference_index > self._reference_index:
+            if next_reference_index > self.reference_index:
                 self.bars[next_reference_index % self.buf_size] = Bar.from_trade(empty_trade)
-                self._reference_index = next_reference_index
+                self.reference_index = next_reference_index
             if abs(trade.quantity - need) < 2 * 1e-13: # trade.quantity = need                
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade
                 else:
                     bar = Bar.from_trade(trade)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 bar.enclose(trade.timestamp)
-                self._finished_bars += 1
-                logger.info(f"Finished {self._finished_bars}'th quote volume bar: {bar}")
+                self.finished_bars += 1
+                logger.info(f"Finished {self.finished_bars}'th quote volume bar: {bar}")
                 self._accumulator += need * trade.price + 1e-13
-                self._reference_index += 1
+                self.reference_index += 1
                 trade.quantity = 0
             elif trade.quantity < need:
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade
                 else:
                     bar = Bar.from_trade(trade)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 self._accumulator += trade.quantity * trade.price
                 trade.quantity = 0
                 
             elif trade.quantity > need:
                 trade_fraction = trade.model_copy(deep=False)
                 trade_fraction.quantity = need
-                if bar := self.bars[self._reference_index % self.buf_size]:
+                if bar := self.bars[self.reference_index % self.buf_size]:
                     bar += trade_fraction
                 else:
                     bar = Bar.from_trade(trade_fraction)
-                    self.bars[self._reference_index % self.buf_size] = bar
+                    self.bars[self.reference_index % self.buf_size] = bar
                 bar.enclose(trade.timestamp)
                 bar.next_id = trade.id
-                self._finished_bars += 1
-                logger.info(f"Finished {self._finished_bars}'th quote volume bar: {bar}")
+                self.finished_bars += 1
+                logger.info(f"Finished {self.finished_bars}'th quote volume bar: {bar}")
                 trade.quantity -= need
                 self._accumulator += need * trade.price + 1e-13
             else:
